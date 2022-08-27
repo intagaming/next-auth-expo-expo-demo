1f00d46ef28ec1a53c5f439d633c422e3c6ece5a
An Hoang <xuanan2001@pm.me>
2022-08-28 06:35:37 +0700

    showing session status

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SessionProvider, useSession } from "next-auth/expo";
 
-export default function App() {
+const InnerApp = () => {
+  const { data: session, status } = useSession();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Session status: {status}</Text>
       <StatusBar style="auto" />
     </View>
+  );
+};
+
+export default function App() {
+  return (
+    <SessionProvider>
+      <InnerApp />
+    </SessionProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
